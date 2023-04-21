46d55d81fb64cfa665c325cda8c28fd60461e20c
adels <adels@ADLIN>
2023-04-21 18:41:09 +0300

    Добавьте файлы проекта.

---
+++ пр7/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace пр7
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            double x = double.Parse(textBox1.Text);
+            double y = double.Parse(textBox2.Text);
+            int n = int.Parse(textBox3.Text);
+            int k = int.Parse(textBox4.Text);
+            double z = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= k; j++)
+                {
+                    z += ((5 * x) + (3 * y)) / (Math.Pow(i, 2) * Math.Pow(j, 2));
+                }
+            }
+            textBox5.Text = z.ToString();
+        }
+    }
+}
